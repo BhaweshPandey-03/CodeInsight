38984a4245ce938e5d7a3d0e79016ef9552fbf47
Bhawesh-GoCodeo <bhawesh@gocodeo.com>
2026-04-28 20:56:09 +0530

    frontend ready for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL: import.meta.env.VITE_API_URL + "/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const reviewCode = async ({ code, language }) => {
